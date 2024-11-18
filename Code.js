671aeedb3890046dc475068b94e86b5d5f325dd6
Marvin Mann <marvin1995mann@gmail.com>
2024-11-18 23:36:20 +0100

    refactoring - time to add some comments he?

--- Code.js
+++ Code.js
@@ -1,11 +1,14 @@
 const creditLeft = {
   cornerStone: "A1",
   display: function(sheet){
-    const cellCreditTitle = sheet.getRange(this.cornerStone);
-    const cellCreditValue = cellCreditTitle.offset(1,0);
+    const cornerStone = sheet.getRange(this.cornerStone);
 
-    cellCreditTitle.setBackground("red").setValue("Restliches Geld");
-    cellCreditValue.setBackground("red").setFormula("=SUM(G3:G6)-SUM(B6:B33)");
+    const incomeRange = sheet.getRange(income.cornerStone).offset(2,1).getA1Notation() + ':' + sheet.getRange(income.cornerStone).offset(2+income.spendingListLength,1).getA1Notation();
+
+    const spendingsRange = sheet.getRange(spendings.cornerStone).offset(2,1).getA1Notation() + ':' + sheet.getRange(spendings.cornerStone).offset(2+spendings.spendingListLength,1).getA1Notation();
+
+    cornerStone.setBackground("red").setValue("Restliches Geld");
+    cornerStone.offset(1,0).setBackground("red").setFormula("=SUM(" + incomeRange + ")-SUM(" + spendingsRange + ")");
   }
 }
 
@@ -13,12 +16,8 @@ const spendings = {
   cornerStone: "A4",
   spendingListLength : 100,
   headers: ["Beschreibung", "Betrag", "Wiederkehrend", "Start Monat"],
-  typesList: { 
-    range: ['Ausstehend', 'Monatlich', 'Zweimonatlich', 'Vierteljährlich', 'Halbjährlich', 'Jährlich']
-  },
-  monthsList: { 
-    range: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
-  },
+  recurringTypes: ['Ausstehend', 'Monatlich', 'Zweimonatlich', 'Vierteljährlich', 'Halbjährlich', 'Jährlich'],
+  months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
   display: function(sheet) {
     const titleCell = sheet.getRange(this.cornerStone).setValue("Ausgaben");
     
@@ -38,14 +37,14 @@ const spendings = {
     
     typeRange.setDataValidation(
       SpreadsheetApp.newDataValidation()
-        .requireValueInList(this.typesList.range)
+        .requireValueInList(this.recurringTypes)
         .setAllowInvalid(false)
         .build()
     );
     
     monthRange.setDataValidation(
       SpreadsheetApp.newDataValidation()
-        .requireValueInList(this.monthsList.range)
+        .requireValueInList(this.months)
         .setAllowInvalid(false)
         .build()
     );
@@ -65,17 +64,10 @@ const spendings = {
 
 const income = {
   cornerStone: "F1",
+  spendingListLength: 4,
   headers: ["Beschreibung", "Betrag", "Wiederkehrend", "Start Monat"],
-  typesList: { 
-    range: ['Ausstehend', 'Monatlich', 'Zweimonatlich', 'Vierteljährlich', 'Halbjährlich', 'Jährlich'],
-    start: "H3",
-    end: "H6"
-  },
-  monthsList: { 
-    range: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
-    start: "I3",
-    end: "I6"
-  },
+  recurringTypes: ['Ausstehend', 'Monatlich', 'Zweimonatlich', 'Vierteljährlich', 'Halbjährlich', 'Jährlich'],
+  months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
   display: function(sheet) {
     const titleCell = sheet.getRange(this.cornerStone).setValue("Einkommen");
     
@@ -88,42 +80,34 @@ const income = {
     this.applyDataValidation(sheet);
   },
   applyDataValidation: function(sheet) {
-    const typeRange = sheet.getRange(`${this.typesList.start}:${this.typesList.end}`);
-    const monthRange = sheet.getRange(`${this.monthsList.start}:${this.monthsList.end}`);
+    const cornerStone = sheet.getRange(this.cornerStone);
+    const typeRange = sheet.getRange(cornerStone.offset(2, 2).getA1Notation() + ':' + cornerStone.offset(2+this.spendingListLength, 2).getA1Notation());
+    const monthRange = sheet.getRange(cornerStone.offset(2, 3).getA1Notation() + ':' + cornerStone.offset(2+this.spendingListLength, 3).getA1Notation());
     
     typeRange.setDataValidation(
       SpreadsheetApp.newDataValidation()
-        .requireValueInList(this.typesList.range)
+        .requireValueInList(this.recurringTypes)
         .setAllowInvalid(false)
         .build()
     );
     
     monthRange.setDataValidation(
       SpreadsheetApp.newDataValidation()
-        .requireValueInList(this.monthsList.range)
+        .requireValueInList(this.months)
         .setAllowInvalid(false)
         .build()
     );
   },
   parse: function(sheet, previousSheet){
-    const titleCell = sheet.getRange(this.cornerStone);
-    previousSheet.getRange(`${this.monthsList.start}:${this.monthsList.end}`).getValues().forEach(
-    (_a, index) => {
-      const entryName = previousSheet.getRange(this.monthsList.start).offset(index, -3)
-      const entryAmount = previousSheet.getRange(this.monthsList.start).offset(index, -2)
-      const entryRecurring = previousSheet.getRange(this.monthsList.start).offset(index, -1)
-      const entryMonth = previousSheet.getRange(this.monthsList.start).offset(index, 0)
-      if (entryName.getValue() !== "" && entryName.getValue() !== null) {
-        if (entryAmount.getValue() !== "" && entryAmount.getValue() !== null) {
-          if (entryRecurring.getValue() !== "" && entryRecurring.getValue() !== null) {
-            sheet.getRange(entryName.getA1Notation()).setValue(entryName.getValue())
-            sheet.getRange(entryAmount.getA1Notation()).setValue(entryAmount.getValue())
-            sheet.getRange(entryRecurring.getA1Notation()).setValue(entryRecurring.getValue())
-            sheet.getRange(entryMonth.getA1Notation()).setValue(entryMonth.getValue())
-          }
-        } 
-      }
-    })
+    const prevCornerStone = previousSheet.getRange(this.cornerStone);
+    const cornerStone = sheet.getRange(this.cornerStone);
+    let prevEntries = previousSheet.getRange(prevCornerStone.offset(2,0).getA1Notation() + ':' + prevCornerStone.offset(2+this.spendingListLength, 3).getA1Notation()).getValues();
+
+    prevEntries = prevEntries.filter(obj => obj[2] !== '' && obj[2] !== null)
+                    .sort((first, second) => first[1] - second[1])
+                      .reverse()
+    
+    sheet.getRange(cornerStone.offset(2, 0).getA1Notation() + ':' + cornerStone.offset(2+prevEntries.length-1, 3).getA1Notation()).setValues(prevEntries);
   }
 }
 
@@ -142,7 +126,10 @@ const motivation = {
 
 function createSheetName(){
   const dateObj = new Date();
-  return `${dateObj.getUTCFullYear()}/${dateObj.getUTCMonth() + 1}/${dateObj.getUTCMinutes()}/${dateObj.getUTCSeconds()}`;
+  const name = `${dateObj.getUTCFullYear()}/${dateObj.getUTCMonth() + 1}/${dateObj.getUTCMinutes()}/${dateObj.getUTCSeconds()}`;
+  // const name = `${dateObj.getUTCFullYear()}/${dateObj.getUTCMonth() + 1}`;
+  Logger.log("createSheetName() returns the following string: " + name);
+  return name;
 }
 
 function newMonthNewTab(){
